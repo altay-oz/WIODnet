566245f3b1b6a377efc2dce9448202b3499717fd
altay <altay@ikaria>
2019-10-14 00:03:39 +0200

    Correct tests.

--- tests/testthat/test-get_tech_intensity.R
+++ tests/testthat/test-get_tech_intensity.R
@@ -1,6 +1,10 @@
 test_that("create technology intensity dataframe", {
 
-    tech.int <<- getTechIntensity(yearly.mini.raw)
+    ## loading the test data of the year 2002 WIOD for exchanges
+    ## between FRA and USA
+    yearly.mini.raw <- get(load("./w2002.rda"))
+
+    tech.int <- getTechIntensity(yearly.mini.raw)
 
     expect_equal(dim(tech.int), c(69,2))
     expect_equal(names(tech.int), c("RNr", "NewIndustryCode"))
