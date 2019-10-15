b593e950b71d32608986fc53a1d245dbd66a8249
Keungoui Kim <awekimm@gmail.com>
2019-10-15 10:06:36 +0200

    Add test for getNetLong

--- tests/testthat/test-get_net_long.R
+++ tests/testthat/test-get_net_long.R
@@ -1,10 +1,15 @@
 test_that("obtain long network table", {
 
     ## network matrix
-    w2002.IO <<- get(load("./wide_IO_w2002.rda"))
+    w2002.IO <<- get(load("C:/Users/Kengoui KIM/Desktop/WIODnet-master/tests/testthat/wide_IO_w2002.rda"))
     
     mini.long <- getNetLong(w2002.IO)
     
+    ## check dimension of data
     expect_equal(dim(mini.long), c(6888, 3))
 
+    ## check list of nodes (country-industry)
+    expect_equal(
+        w2002.IO %>% mutate(ID=paste(Country,IndustryCode,sep=".")) %>% select(ID) %>% unique %>% nrow,
+        combine(unique(mini.long$source), unique(mini.long$target)) %>% data.frame %>% filter(substr(.,5,5)!="Z")  %>% unique  %>% nrow)
 })
