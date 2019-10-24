ee89650fa94a57a2e871e089979280544a024fcc
altay <altay@ikaria>
2019-10-24 17:05:31 +0200

    Correcting the function dealing with network calculations.

--- R/calculate_network.R
+++ R/calculate_network.R
@@ -27,12 +27,12 @@ netWIOD <- function(file.dir =  "./wiod_long_data") {
     ## creating the list of files
     wiod.long.file.name <- list.files(file.dir, pattern="^wiod_long", full.names = TRUE)
     ## creating where all yearly network analysis are stored
-    network.data.dir <<- "./wiod_network_data"
+    network.data.dir <- "./wiod_network_data"
     dir.create(network.data.dir, showWarnings = FALSE)
     
     ## call all functions above with this line, creating a final long file
     ## wiod_long_YEAR.csv to perform network analysis.
-    lapply(wiod.long.file.name, netCalcWrite)
+    lapply(wiod.long.file.name, netCalcWrite, network.data.dir = network.data.dir, ctry = 0)
 }
 
 
@@ -45,18 +45,30 @@ globalVariables(c("network.data.dir", "weight", "target", "V"))
 #'
 #' @param wiod.long.file.name rda file containing the long network table
 #'     to read.
+#'
+#' @param network.data.dir directory where all network calculation files are stored. 
+#'
+#' @param ctry control variable if the analysis is country based, 0 or 1.
 #' 
-netCalcWrite  <- function(wiod.long.file.name) {
+netCalcWrite  <- function(wiod.long.file.name, network.data.dir, ctry) {
 
-    year <- substr(wiod.long.file.name, 28, 31)
+    if (ctry == 0) {
+        year <- substr(wiod.long.file.name, 28, 31)
+    } else if (ctry == 1) {
+        year <- substr(wiod.long.file.name, 38, 41)
+    }
     
     yearly.long.wiod <- get(load(wiod.long.file.name))
     
-    nc <- netCalc(yearly.long.wiod)
+    nc <- netCalc(yearly.long.wiod, ctry)
 
     ## create file names
-    file.name <- paste0(paste("wiod_network_scores", year, sep = "_"), ".rda")
-
+    if (ctry == 0) {
+        file.name <- paste0(paste("wiod_network_scores", year, sep = "_"), ".rda")
+    } else if (ctry == 1) {
+        file.name <- paste0(paste("wiod_ctry_network_scores", year, sep = "_"), ".rda")
+    }
+        
     dir.file.name <- paste(network.data.dir, file.name, sep="/")
     ## write files with year
     save(nc, file = dir.file.name)
@@ -75,50 +87,59 @@ netCalcWrite  <- function(wiod.long.file.name) {
 #' @param wiod.net.df the long table data frame on which all network
 #'     calculations are done.
 #' 
-netCalc <- function(wiod.net.df) {
+netCalc <- function(wiod.net.df, ctry) {
 
     ## removing 0.05 million $ connections.
     yearly.wiod <- wiod.net.df %>% filter(weight > 0.05)
 
     ## ungroup
     yearly.wiod <- ungroup(yearly.wiod)
-    
-    wiod.nodes.t <- yearly.wiod %>% select(target) %>% unique %>% transmute(country.ind=target)
-    wiod.nodes.f <- yearly.wiod %>% select(source) %>% unique %>% transmute(country.ind=source)
+
+    wiod.nodes.t <- yearly.wiod %>% select(target) %>% unique %>% rename(node.name = target)
+    wiod.nodes.f <- yearly.wiod %>% select(source) %>% unique %>% rename(node.name = source)
     wiod.nodes <- rbind(wiod.nodes.t, wiod.nodes.f) %>% unique
 
+
+    if (ctry == 1) {
+        node.type <- "country"
+        names(wiod.nodes) <- "country"
+    } else if (ctry == 0) {
+        node.type <- "country.ind"
+        names(wiod.nodes) <- "country.ind"
+    }
+    
     g <- graph.data.frame(yearly.wiod, directed = TRUE, vertices=wiod.nodes)
 
     ## node strength
     strength.all <- strength(g, vids = V(g), mode = c("all"), loops = TRUE)
-    strength.all <- rownames_to_column(as.data.frame(strength.all), var = "country.ind")
+    strength.all <- rownames_to_column(as.data.frame(strength.all), var = node.type)
     strength.all[is.na(strength.all)] <- 0
     
     strength.out <- strength(g, vids = V(g), mode = c("out"), loops = TRUE)
-    strength.out <- rownames_to_column(as.data.frame(strength.out), var = "country.ind")
+    strength.out <- rownames_to_column(as.data.frame(strength.out), var = node.type)
     strength.out[is.na(strength.out)] <- 0
     
     strength.in <- strength(g, vids = V(g), mode = c("in"), loops = TRUE)
-    strength.in <- rownames_to_column(as.data.frame(strength.in), var = "country.ind")
+    strength.in <- rownames_to_column(as.data.frame(strength.in), var = node.type)
     strength.in[is.na(strength.in)] <- 0
     
     ## betweenness
     btw <- betweenness(g, v = V(g), directed = TRUE)
-    btw <- rownames_to_column(as.data.frame(btw), var = "country.ind")
+    btw <- rownames_to_column(as.data.frame(btw), var = node.type)
     btw[is.na(btw)] <- 0
     
     ## page.rank
     page.rank <- page_rank(g, damping = 0.999)$vector
-    page.rank <- rownames_to_column(as.data.frame(page.rank), var = "country.ind")
+    page.rank <- rownames_to_column(as.data.frame(page.rank), var = node.type)
     page.rank[is.na(page.rank)] <- 0
     
     ## eigen centrality
     ## https://lists.nongnu.org/archive/html/igraph-help/2015-11/msg00020.html
     eigen.cent <- eigen_centrality(g, directed = TRUE)$vector
-    eigen.cent <- rownames_to_column(as.data.frame(eigen.cent), var = "country.ind")
+    eigen.cent <- rownames_to_column(as.data.frame(eigen.cent), var = node.type)
     eigen.cent[is.na(eigen.cent)] <- 0
     
-    net.scores <- Reduce(function(x, y) merge(x = x, y = y, by = "country.ind",
+    net.scores <- Reduce(function(x, y) merge(x = x, y = y, by = node.type,
                                               all.x = TRUE),
                          list(wiod.nodes, strength.all,
                               strength.out, strength.in, btw,
--- R/get_country_panel.R
+++ R/get_country_panel.R
@@ -2,21 +2,39 @@
 #'
 #' @description Create country based long tables and merge them them to
 #'     create a panel data in csv form based on downloaded and prapared
-#'     WIOD data.
+#'     WIOD data. If there is no WIOD data downloaded, this function
+#'     will donwload it, unzip it and prepare long tables.
 #' 
 #' @usage getCountryWIOD(country.long.dir = "./wiod_long_data")
 #'
+#' @param long.dir directory where the long tables are already obtained, Default: "./wiod_long_data"
+#' 
 #' @param country.long.dir directory where the long tables are recorded, Default: "./wiod_country_long_data"
-
+#'
 #' @examples
-#' # Merge all long tables using default values.
+#' # Create country based panel data.
 #' \dontrun{getCountryWIOD()}
 #'
 #' @export
 #'
-getCountryWIOD <- function(country.long.dir = "./wiod_country_long_data") {
+getCountryWIOD <- function(long.dir =  "./wiod_long_data",
+                           country.long.dir = "./wiod_ctry_long_data",
+                           ctry.merge.dir = "./yearly_ctry_merged_data") {
 
-    lapply(seq(2000, 2014), getCountryLong)
+    ## if the default wiod_long_data or the same long.dir directory is
+    ## not present then download the original WIOD zip file and open it
+    ## and create long tables at the designated long.dir.
+    if(!file.exists(long.dir)) {
+        message("There is no wiod long data directory.")
+        message("Starting to download the original zip file and creating long tables.")
+        getWIOD()
+    }
+
+    dir.create(country.long.dir, showWarnings = FALSE)
+    dir.create(ctry.merge.dir, showWarnings = FALSE)
+    
+    lapply(seq(2000, 2014), getCtryLong, long.dir = "./wiod_long_data",
+                        country.long.dir = "./wiod_ctry_long_data")
 
     wiod.ctry.long.file.name <- list.files(country.long.dir,
                                            pattern="^wiod_ctry_long", full.names = TRUE)
@@ -25,21 +43,19 @@ getCountryWIOD <- function(country.long.dir = "./wiod_country_long_data") {
     network.ctry.data.dir <- "./wiod_ctry_network_data"
     dir.create(network.ctry.data.dir, showWarnings = FALSE)
     
-    ## call all functions above with this line, creating a final long file
-    ## wiod_long_YEAR.csv to perform network analysis.
-    lapply(wiod.ctry.long.file.name, netCalcCtryWrite,
-           network.ctry.data.dir = "./wiod_ctry_network_data")
+    ## creating yearly network calculation files where all nodes are countries.
+    lapply(wiod.ctry.long.file.name, netCalcWrite,
+           network.data.dir = "./wiod_ctry_network_data", ctry = 1)
 
-
-    lapply(seq(2000, 2014), bindCtryFiles, long.dir = "./wiod_country_long_data",
+    ## binding all files yearly.
+    lapply(seq(2000, 2014), bindCtryFiles, long.dir = "./wiod_ctry_long_data",
                        net.dir = "./wiod_ctry_network_data",
-                       merge.dir = "./yearly_ctry_merged_data")
+                       merge.dir = ctry.merge.dir)
 
-    
     ## obtaining the last file to use in econometric study.
-    ## rbinding all yearly network score VA value files
+    ## rbinding all yearly network score VA values etc.
     final.ctry.wiod.df <- do.call(rbind,
-                             lapply(list.files(path = merge.dir, full.names = TRUE),
+                             lapply(list.files(path = ctry.merge.dir, full.names = TRUE),
                                     read.csv))
 
     write.csv(final.ctry.wiod.df, "wiod_ctry_net_panel_2000_2014.csv", row.names = FALSE)
@@ -47,83 +63,9 @@ getCountryWIOD <- function(country.long.dir = "./wiod_country_long_data") {
     message("Country based panel data is created.")
 }
 
-# to use netCalcWrite once corrected
-netCalcCtryWrite  <- function(wiod.long.file.name,
-                              network.ctry.data.dir = "./wiod_ctry_network_data") {
 
-    year <- substr(wiod.long.file.name, 41, 44)
-
-    yearly.long.wiod <- get(load(wiod.long.file.name))
-
-    nc <- netCtryCalc(yearly.long.wiod)
-
-    ## create file names
-    file.name <- paste0(paste("wiod_ctry_network_scores", year, sep = "_"), ".rda")
-
-    dir.file.name <- paste(network.ctry.data.dir, file.name, sep="/")
-    ## write files with year
-    save(nc, file = dir.file.name)
-
-    ## just printing where we are.
-    message(paste("Country based network file for the year", year,
-                  "is ready."))
-    
-}
-
-
-netCtryCalc <- function(wiod.net.df) {
-    
-    ## removing 0.05 million $ connections.
-    yearly.wiod <- wiod.net.df %>% filter(weight > 0.05)
-
-    yearly.wiod <- ungroup(yearly.wiod)
-    
-    wiod.nodes.t <- yearly.wiod %>% select(target) %>% unique %>% rename(country = target)
-    wiod.nodes.f <- yearly.wiod %>% select(source) %>% unique %>% rename(country=source)
-    wiod.nodes <- rbind(wiod.nodes.t, wiod.nodes.f) %>% unique
-    
-    g <- graph.data.frame(yearly.wiod, directed = TRUE, vertices=wiod.nodes)
-
-    ## node strength
-    strength.all <- strength(g, vids = V(g), mode = c("all"), loops = TRUE)
-    strength.all <- rownames_to_column(as.data.frame(strength.all), var = "country")
-    strength.all[is.na(strength.all)] <- 0
-    
-    strength.out <- strength(g, vids = V(g), mode = c("out"), loops = TRUE)
-    strength.out <- rownames_to_column(as.data.frame(strength.out), var = "country")
-    strength.out[is.na(strength.out)] <- 0
-    
-    strength.in <- strength(g, vids = V(g), mode = c("in"), loops = TRUE)
-    strength.in <- rownames_to_column(as.data.frame(strength.in), var = "country")
-    strength.in[is.na(strength.in)] <- 0
-    
-    ## betweenness
-    btw <- betweenness(g, v = V(g), directed = TRUE)
-    btw <- rownames_to_column(as.data.frame(btw), var = "country")
-    btw[is.na(btw)] <- 0
-    
-    ## page.rank
-    page.rank <- page_rank(g, damping = 0.999)$vector
-    page.rank <- rownames_to_column(as.data.frame(page.rank), var = "country")
-    page.rank[is.na(page.rank)] <- 0
-    net.scores <- Reduce(function(x, y) merge(x = x, y = y,
-                                              by = "country", all.x = TRUE),
-                         list(wiod.nodes, strength.all, strength.out,
-                              strength.in, btw, page.rank))
-    
-    return(net.scores)
-}
-
-
-getCountryLong <- function(year, long.dir = "./wiod_long_data",
-                        country.long.dir = "./wiod_country_long_data") {
-
-    ## if the default wiod_long_data or the same long.dir directory is
-    ## not present then download the original WIOD zip file and open it
-    ## and create long tables at the designated long.dir.
-    if(!file.exists(long.dir)) {
-        getWIOD()
-    }
+getCtryLong <- function(year, long.dir = "./wiod_long_data",
+                        country.long.dir = "./wiod_ctry_long_data") {
 
     ## long.dir where all long data with country.ind is located
     ## country.long.dir where all long data only with country will be located
@@ -161,16 +103,13 @@ getCountryLong <- function(year, long.dir = "./wiod_long_data",
                   "are ready."))
 }
 
-
 bindCtryFiles <- function(year,
-                       long.dir = "./wiod_country_long_data",
+                       long.dir = "./wiod_ctry_long_data",
                        net.dir = "./wiod_ctry_network_data",
                        merge.dir = "./yearly_ctry_merged_data") {
     ## bind the network scores, VA values and domestic and internation
     ## trade based on year and country.industry level.
 
-    dir.create(merge.dir, showWarnings = FALSE)
-    
     net.score.file <- paste0(net.dir, "/wiod_ctry_network_scores_", year, ".rda")
     dom.int.trade.file <- paste0(long.dir, "/dom_int_ctry_long_", year, ".rda")
     VA.file <- paste0(long.dir, "/VA_ctry_long_", year, ".rda")
